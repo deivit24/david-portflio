a0b9a577de98f6fdcd5a0c822a04a1e97b43fd2f
David Salazar <david.asal@hotmail.com>
2019-01-01 22:16:18 -0600

    first upload of my new portfolio

---
+++ dist/js/main.js
@@ -0,0 +1,124 @@
+// select DOM Items
+
+const menuBtn = document.querySelector('.menu-btn');
+const menu = document.querySelector('.menu');
+const menuNav = document.querySelector('.menu-nav');
+const menuBranding = document.querySelector('.menu-branding');
+const navItem = document.querySelectorAll('.nav-item');
+
+// set initial state of Menu
+
+let showMenu = false;
+
+menuBtn.addEventListener('click', toggleMenu);
+
+function toggleMenu() {
+  if (!showMenu) {
+    menuBtn.classList.add('close');
+    menu.classList.add('show');
+    menuNav.classList.add('show');
+    menuBranding.classList.add('show');
+    navItem.forEach(item => item.classList.add('show'));
+
+    // set menu state
+    showMenu = true;
+  } else {
+    menuBtn.classList.remove('close');
+    menu.classList.remove('show');
+    menuNav.classList.remove('show');
+    menuBranding.classList.remove('show');
+    navItem.forEach(item => item.classList.remove('show'));
+
+    // set menu state
+    showMenu = false;
+
+  }
+}
+// Change my Name
+
+setInterval(function () {
+  // toggle the class every five second
+  $('#david').toggleClass('change');
+  setTimeout(function () {
+    // toggle back after 1 second
+    $('#david').toggleClass('change');
+  }, 1000);
+
+}, 3000);
+
+const TypeWriter = function (txtElement, words, wait = 3000) {
+  this.txtElement = txtElement;
+  this.words = words;
+  this.txt = '';
+  this.wordIndex = 0;
+  this.wait = parseInt(wait, 10);
+  this.type();
+  this.isDeleting = false;
+};
+
+// Type Method
+TypeWriter.prototype.type = function () {
+  // Current Index of Word
+  const current = this.wordIndex % this.words.length;
+  // Get Full Text od Current Word
+  const fullTxt = this.words[current];
+
+  // CHeck if deleting
+  if (this.isDeleting) {
+    // Remove Character
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+  } else {
+    // Add Character
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
+  }
+
+  // Insert txt into Element
+  this.txtElement.innerHTML = `<span class="text">${this.txt}</span>`;
+
+  // Initial Type Speed
+  let typeSpeed = 200;
+
+  if (this.isDeleting) {
+    typeSpeed /= 2;
+  }
+
+  // If word is complete
+  if (!this.isDeleting && this.txt === fullTxt) {
+    // this will make pause
+    typeSpeed = this.wait;
+    // Set delete to true
+    this.isDeleting = true;
+  } else if (this.isDeleting && this.txt === '') {
+    this.isDeleting = false;
+    // move to next word
+    this.wordIndex++;
+    // Pause before start typing
+    typeSpeed = 200;
+  }
+
+  setTimeout(() => this.type(), typeSpeed);
+};
+// Init on DOM Load
+
+document.addEventListener('DOMContentLoaded', init);
+
+// Init App
+
+function init() {
+  const txtElement = document.querySelector('#sec1 .txt-type');
+  const words = JSON.parse(txtElement.getAttribute('data-words'));
+  const wait = txtElement.getAttribute('data-wait');
+  // Init TypeWriter
+  new TypeWriter(txtElement, words, wait);
+}
+
+
+// Scroll Between Sections
+$(function () {
+  $('a[href*="#"]').on('click', function (e) {
+    e.preventDefault();
+    $('html, body').animate({
+      scrollTop: $($(this).attr('href')).offset().top
+    }, 500, 'linear');
+  });
+});
